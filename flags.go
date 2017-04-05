2680000176dcbbdff903e2033eb12f401d206e3f
Rob Smith <kormoc@gmail.com>
2017-04-04 23:00:37 -0700

    order

--- flags.go
+++ flags.go
@@ -28,6 +28,7 @@ func processFlags() {
 	flag.BoolVar(&skipCreate, "skipCreate", false, "Skip creating new hashes. Useful for just validating existing hashes")
 	flag.BoolVar(&skipValidation, "skipValidation", false, "Skip validating existing hashes. Useful to just generate for new files")
 	flag.BoolVar(&updateOnNewMTime, "updateOnNewMTime", false, "Update hashes if mtime is newer then last check time")
+	flag.BoolVar(&version, "version", false, "Display the version")
 	flag.Int64Var(&mtimeSettle, "mtimeSettle", 1800, "Don't create a hash until the mtime is at least this many seconds old")
 	flag.IntVar(&bufferSize, "bufferSize", 1024*1024*8, "Read buffer size")
 	flag.IntVar(&ioniceClass, "ioniceClass", int(ionice.IOPRIO_CLASS_IDLE), "ionice class. 0: none, 1: realtime, 2: best-effort, 3: idle")
@@ -40,7 +41,6 @@ func processFlags() {
 	flag.StringVar(&logfileLevel, "logfileLevel", "warn", "Log level for log file. error, warn, verbose, debug")
 	flag.StringVar(&logfilePath, "logfilePath", "", "Path to logfile")
 	flag.StringVar(&xattrRoot, "xattrRoot", "user.checksum.", "base xattr path for checksums")
-	flag.BoolVar(&version, "version", false, "Display the version")
 
 	flag.Parse()
 }
